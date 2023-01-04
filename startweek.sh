0a40bd352d28096a3450bb997ca2ac51bcd7c08d
zeturn <62530500+zeturn@users.noreply.github.com>
2023-01-04 16:57:16 +0800

    Create startweek.sh

---
+++ startweek.sh
@@ -0,0 +1,15 @@
+python3 ./hotresearch/hotresearch.py
+
+
+year=`date +%Y `
+month=`date +%m `
+day=`date +%d `
+hour=`date +%H`
+now=$year-$month-$day-$hour
+
+
+git config --global user.email ""
+git config --global user.name "actioner"
+
+git add .
+git commit -m "$now"
